b102893f8edc953f135c2748c4eba6250289c052
hieubkvn123 <hieubkvn123@gmail.com>
2025-01-23 12:32:37 +0800

    Checkpoint

--- create_experiments.py
+++ create_experiments.py
@@ -1,7 +1,18 @@
 import os
 
-python train.py --dataset gaussian200 --k 1 --outfile results/ablation_study_values_of_k2.csv --batch_size 10 --num_batches 1000
+# Command template
+EXP_TEMPLATE = 'python3 train_gaussian.py --dataset gaussian{N} --k {k} --outfile results/{fname}.csv --batch_size 64 --num_tuples {n_tuples}\n'
 
-def ablation_study_values_of_N
+# Hyper-parameters
+ks = list(range(42, 201, 2))
+Ns = [500, 600, 700, 800]
+exp_file = 'bash/additional_experiments.sh'
+fname = 'results/additional_experiments.csv'
 
+if __name__ == '__main__':
+    with open(exp_file, 'w') as f:
+        for N in Ns:
+            for k in ks:
+                cmd = EXP_TEMPLATE.format(N=N, k=k, fname=fname, n_tuples=N**2)
+                f.write(cmd)
 
--- plot_result.py
+++ plot_result.py
@@ -1,63 +1,37 @@
 import os
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from argparse import ArgumentParser
 
-def check_unique_xaxis(x):
-    return len(x) == len(set(x))
+RESULT_FILE1 = 'results/ablation_study_values_of_k.csv'
 
-def plot_simple_line(x, y, outfile, figsize=(15, 7)):
+def plot_result1(output, figsize=(12, 7)):
+    if not os.path.exists(RESULT_FILE1):
+        print(f'File {RESULT_FILE1} does not exist...')
+        return
+
+    # Read the data file
+    df = pd.read_csv(RESULT_FILE1)
+    df['N'] = np.sqrt(df['num_tuples']).astype(int)
+
+    # Initialize figure
     fig, ax = plt.subplots(figsize=figsize)
-    ax.plot(x, y)
+    for N in np.unique(df['N']):
+        tmp = df[df['N'] == N]
+        ax.plot(tmp['k'], tmp['gen_gap'], linestyle='-.', marker='o', label=f'$N=${N}')
 
-    # Adding labels and title
-    plt.xlabel('X values')
-    plt.ylabel('Y values')
-    plt.title('Error bar plot')
+    # Axes labels
+    ax.set_xlabel('Number of negative samples ($k$)')
+    ax.set_ylabel("Generalization gap")
 
-    # Show grid for better readability
-    plt.grid(True)
-
-    # Legend + save
+    # Plot 
     plt.legend()
-    plt.savefig(outfile, bbox_inches='tight')
-
-def plot_errbar(x, y_mean, y_std, outfile, figsize=(15, 7)):
-    fig, ax = plt.subplots(figsize=figsize)
-    ax.errorbar(x, y_mean, yerr=y_std, fmt='-o', capsize=3, color='b', label='Data with error bars', markersize=8)
-    
-    # Adding labels and title
-    plt.xlabel('X values')
-    plt.ylabel('Y values')
-    plt.title('Error bar plot')
-
-    # Show grid for better readability
-    plt.grid(True)
-
-    # Legend + save
-    plt.legend()
-    plt.savefig(outfile, bbox_inches='tight')
-
+    plt.grid()
+    plt.tight_layout()
+    plt.savefig(output)
+    print(f'Output saved to {output}.')
+         
 
 if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser.add_argument('--outfile', type=str, required=True, help='Path to experiment result file')
-    parser.add_argument('--resultfile', type=str, required=False, default='result.png', help='Path to visualization')
-    parser.add_argument('--xaxis', type=str, required=False, default='k', help='Data at the x-axis')
-    parser.add_argument('--yaxis', type=str, required=False, default='gen_gap', help='Data at the y-axis')
-    args = vars(parser.parse_args())
-
-    if os.path.exists(args['outfile']):
-        df = pd.read_csv(args['outfile'])
-        xaxis, yaxis = args['xaxis'], args['yaxis']
-
-        # Plot simple line
-        if check_unique_xaxis(df[xaxis].to_list()):
-            plot_simple_line(df[xaxis], df[yaxis], args['resultfile'])
-        else: # Plot error bar
-            mean = df.groupby(xaxis).mean()[yaxis]
-            std  = df.groupby(xaxis).std()[yaxis].fillna(0)
-            x    = mean.index
-            plot_errbar(x, mean, std, args['resultfile'])
-    else:
-        print(f'File args["outfile"] does not exist')
+    plot_result1(output='result.png')
