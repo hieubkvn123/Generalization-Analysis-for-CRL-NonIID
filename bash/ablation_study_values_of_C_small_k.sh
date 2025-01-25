0487423ef24e3383cec7ecb38cc268484904b41e
hieubkvn123 <hieubkvn123@gmail.com>
2025-01-25 14:42:21 +0800

    Checkpoint

---
+++ bash/ablation_study_values_of_C_small_k.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+python3 train_gaussian.py --k 3 --outfile test.csv --batch_size 64 --C 5 --outfile results/ablation_study_values_of_C_small_k.csv
+python3 train_gaussian.py --k 3 --outfile test.csv --batch_size 64 --C 10 --outfile results/ablation_study_values_of_C_small_k.csv
+python3 train_gaussian.py --k 3 --outfile test.csv --batch_size 64 --C 15 --outfile results/ablation_study_values_of_C_small_k.csv
+python3 train_gaussian.py --k 3 --outfile test.csv --batch_size 64 --C 20 --outfile results/ablation_study_values_of_C_small_k.csv
+python3 train_gaussian.py --k 3 --outfile test.csv --batch_size 64 --C 25 --outfile results/ablation_study_values_of_C_small_k.csv
+python3 train_gaussian.py --k 3 --outfile test.csv --batch_size 64 --C 30 --outfile results/ablation_study_values_of_C_small_k.csv
+python3 train_gaussian.py --k 3 --outfile test.csv --batch_size 64 --C 35 --outfile results/ablation_study_values_of_C_small_k.csv
+python3 train_gaussian.py --k 3 --outfile test.csv --batch_size 64 --C 40 --outfile results/ablation_study_values_of_C_small_k.csv
+python3 train_gaussian.py --k 3 --outfile test.csv --batch_size 64 --C 45 --outfile results/ablation_study_values_of_C_small_k.csv
+python3 train_gaussian.py --k 3 --outfile test.csv --batch_size 64 --C 50 --outfile results/ablation_study_values_of_C_small_k.csv
---
+++ bash/ablation_study_values_of_k_small_C.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+python3 train_gaussian.py --k 5 --outfile test.csv --batch_size 64 --C 10 --outfile results/ablation_study_values_of_C_small_k.csv
+python3 train_gaussian.py --k 10 --outfile test.csv --batch_size 64 --C 10 --outfile results/ablation_study_values_of_C_small_k.csv
+python3 train_gaussian.py --k 15 --outfile test.csv --batch_size 64 --C 10 --outfile results/ablation_study_values_of_C_small_k.csv
+python3 train_gaussian.py --k 20 --outfile test.csv --batch_size 64 --C 10 --outfile results/ablation_study_values_of_C_small_k.csv
+python3 train_gaussian.py --k 25 --outfile test.csv --batch_size 64 --C 10 --outfile results/ablation_study_values_of_C_small_k.csv
+python3 train_gaussian.py --k 30 --outfile test.csv --batch_size 64 --C 10 --outfile results/ablation_study_values_of_C_small_k.csv
+python3 train_gaussian.py --k 35 --outfile test.csv --batch_size 64 --C 10 --outfile results/ablation_study_values_of_C_small_k.csv
+python3 train_gaussian.py --k 40 --outfile test.csv --batch_size 64 --C 10 --outfile results/ablation_study_values_of_C_small_k.csv
+python3 train_gaussian.py --k 45 --outfile test.csv --batch_size 64 --C 10 --outfile results/ablation_study_values_of_C_small_k.csv
+python3 train_gaussian.py --k 50 --outfile test.csv --batch_size 64 --C 10 --outfile results/ablation_study_values_of_C_small_k.csv
