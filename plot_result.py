a37c63c1c7d2f06d3bcc7b51f75218690d4585fc
hieubkvn123 <hieubkvn123@gmail.com>
2025-01-30 21:31:23 +0800

    Checkpoint

--- plot_result.py
+++ plot_result.py
@@ -4,32 +4,34 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from argparse import ArgumentParser
 
+# Set fonts
+plt.rcParams.update({'font.size': 18})
 RESULT_FILE1 = 'results/ablation_study_iid_vs_noniid_mnist.csv'
 RESULT_FILE2 = 'results/ablation_study_iid_vs_noniid_mnist_only_cls.csv'
 
-def plot_errbar(ax, df, baseline1, baseline2, metric):
+def plot_errbar(ax, df, baseline1, baseline2, metric, label=True):
     tmp = df[df['regime'] == 'subsample']
     
     # Plot the baseline (iid case)
     mean_independent = baseline1[metric].mean()
     std_independent = baseline1[metric].std()
     CI95 = 1.96 * std_independent / np.sqrt(len(baseline1))
-    ax.axhline(y=mean_independent, color='tab:red', label='Baseline ($i.i.d.$ case)')
-    ax.axhline(y=mean_independent - CI95, color='tab:red', linestyle='-.', label='Baseline CI95 ($i.i.d.$ case)')
+    ax.axhline(y=mean_independent, color='tab:red', label='$i.i.d.$ case' if label else None)
+    ax.axhline(y=mean_independent - CI95, color='tab:red', linestyle='-.', label='$i.i.d.$ case ($95\%$ CI)' if label else None)
     ax.axhline(y=mean_independent + CI95, color='tab:red', linestyle='-.')
 
     # Plot the baseline (all-tuples)
     mean_independent = baseline2[metric].mean()
     std_independent = baseline2[metric].std()
     CI95 = 1.96 * std_independent / np.sqrt(len(baseline2))
-    ax.axhline(y=mean_independent, color='tab:green', label='Baseline (all-tuples case)')
-    ax.axhline(y=mean_independent - CI95, color='tab:green', linestyle='-.', label='Baseline CI95 (all-tuples case)')
+    ax.axhline(y=mean_independent, color='tab:green', label='All-tuples' if label else None)
+    ax.axhline(y=mean_independent - CI95, color='tab:green', linestyle='-.', label='All-tuples ($95\%$ CI)' if label else None)
     ax.axhline(y=mean_independent + CI95, color='tab:green', linestyle='-.')
 
     # Plot the sub-sample regime
     mean = tmp.groupby('M')[metric].mean()
     std = tmp.groupby('M')[metric].std()
-    ax.errorbar(mean.index, mean, yerr=std, label='Sub-sample (Non-$i.i.d.$ case)')
+    ax.errorbar(mean.index, mean, yerr=std, label='Non-$i.i.d.$ case' if label else None)
 
     return ax 
 
@@ -45,36 +47,36 @@ def plot_result1(output, figsize=(12, 7)):
     baseline2 = df[df['regime'] == 'all']
 
     # Initialize plot
-    fig, axes = plt.subplots(1, 2, figsize=figsize)
-
-    # Plot errorbars for gen gap
-    axes[0] = plot_errbar(axes[0], df, baseline1, baseline2, metric='gen_gap')
-    axes[0].set_xlabel('Number of tuples used for training ($\mathrm{M}$)')
-    axes[0].set_ylabel("Generalization gap")
-    axes[0].legend()
-    axes[0].grid()    
-
-    # Plot errorbars for gen gap
-    axes[1] = plot_errbar(axes[1], df, baseline1, baseline2, metric='test_acc')
-    axes[1].set_xlabel('Number of tuples used for training ($\mathrm{M}$)')
-    axes[1].set_ylabel("Classifier Accuracy")
+    fig, axes = plt.subplot_mosaic([['left', 'right']], layout='constrained', figsize=figsize)
+    axes['left'].tick_params(axis='both', which='major', labelsize=12)
+    axes['right'].tick_params(axis='both', which='major', labelsize=12)
 
     # Plot the range for cls without CRL
     mean_acc = df_cls['val_acc'].mean()
     std_acc = df_cls['val_acc'].std()
     CI95 = 1.96 * std_acc / np.sqrt(len(df_cls))
-    axes[1].axhline(y=mean_acc, color='tab:orange', label='Baseline (without CRL)')
-    axes[1].axhline(y=mean_acc - CI95, color='tab:orange', linestyle='-.', label='Baseline CI95 (without CRL)')
-    axes[1].axhline(y=mean_acc + CI95, color='tab:orange', linestyle='-.')
-    axes[1].legend()
-    axes[1].grid()    
+    axes['right'].axhline(y=mean_acc, color='tab:orange', label='No-CRL')
+    axes['right'].axhline(y=mean_acc - CI95, color='tab:orange', linestyle='-.', label='No-CRL ($95\%$ CI)')
+    axes['right'].axhline(y=mean_acc + CI95, color='tab:orange', linestyle='-.')
+    axes['right'].grid()    
+
+    # Plot errorbars for gen gap
+    axes['left'] = plot_errbar(axes['left'], df, baseline1, baseline2, metric='gen_gap', label=False)
+    axes['left'].set_xlabel('Number of tuples ($\mathrm{M}$)', fontsize=20)
+    axes['left'].set_ylabel("Generalization gap", fontsize=20)
+    axes['left'].grid()    
+
+    # Plot errorbars for gen gap
+    axes['right'] = plot_errbar(axes['right'], df, baseline1, baseline2, metric='test_acc')
+    axes['right'].set_xlabel('Number of tuples ($\mathrm{M}$)', fontsize=20)
+    axes['right'].set_ylabel("Classifier Accuracy", fontsize=20)
 
     # Plot 
-    plt.tight_layout()
+    fig.legend(loc='outside upper left', mode='expand', ncols=4, fontsize=17)
     plt.savefig(output)
     print(f'Output saved to {output}.')
          
 
 if __name__ == '__main__':
-    plot_result1(output='result.png')
+    plot_result1(output='result.pdf')
     
