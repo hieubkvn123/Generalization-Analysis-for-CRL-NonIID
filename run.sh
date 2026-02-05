fc1bbe2bd5b186599f8f97a316ad85fb49a5006c
hieubkvn123 <hieubkvn123@gmail.com>
2026-02-06 01:26:16 +0700

    Checkpoint

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 # Fashion MNIST
-python3 train_cnn.py  --dataset fashion_mnist --k 3 --M 20000
-python3 train_cnn.py  --dataset fashion_mnist --k 5 --M 20000
-python3 train_cnn.py  --dataset fashion_mnist --k 7 --M 20000
+python3 train_cnn.py  --dataset fashion_mnist --k 3 --M 5000
+python3 train_cnn.py  --dataset fashion_mnist --k 5 --M 5000
+python3 train_cnn.py  --dataset fashion_mnist --k 7 --M 5000
 
 # CIFAR10
-python3 train_cnn.py  --dataset cifar10 --k 3 --M 20000
-# python3 train_cnn.py  --dataset cifar10 --k 5 --M 20000
-python3 train_cnn.py  --dataset cifar10 --k 7 --M 20000
+python3 train_cnn.py  --dataset cifar10 --k 3 --M 5000
+python3 train_cnn.py  --dataset cifar10 --k 5 --M 5000
+python3 train_cnn.py  --dataset cifar10 --k 7 --M 5000
 
 # MNIST 
-python3 train_cnn.py  --dataset mnist --k 3 --M 20000
-python3 train_cnn.py  --dataset mnist --k 5 --M 20000
-python3 train_cnn.py  --dataset mnist --k 7 --M 20000
+python3 train_cnn.py  --dataset mnist --k 3 --M 5000
+python3 train_cnn.py  --dataset mnist --k 5 --M 5000
+python3 train_cnn.py  --dataset mnist --k 7 --M 5000
 
 
